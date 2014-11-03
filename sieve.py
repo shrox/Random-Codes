9660987cf589d47e73c491f79c9936361cbe1b01
Shreedhar Manek <sm123rocks@gmail.com>
2014-11-03 16:23:07 +0530

    command line argument and bisection method

--- sieve.py
+++ sieve.py
@@ -1,9 +1,9 @@
 primes = []
-for i in xrange(2,1000000):
+for i in xrange(2,1000000000000):
 	primes.append(i)
 
 p = 2
-while p!=1000:
+while p!=1000000:
 	for n in xrange(p*p,100,p):
 		if n in primes:
 			primes[primes.index(n)] = 0
